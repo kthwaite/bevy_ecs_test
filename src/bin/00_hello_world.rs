7d62e97c5da9c19e701383e374405a406e7d698f
cthwaite <28732700+cthwaite@users.noreply.github.com>
2021-12-30 17:59:10 +0000

    refactor: Update examples for bevy_ecs 0.5

--- src/bin/00_hello_world.rs
+++ src/bin/00_hello_world.rs
@@ -1,4 +1,4 @@
-use bevy_ecs::{IntoQuerySystem, ParallelExecutor, Resources, Schedule, World};
+use bevy_ecs::prelude::*;
 
 // Stage label.
 const MAIN: &'static str = "MAIN";
@@ -11,26 +11,15 @@ fn hello_world() {
 fn main() {
     // 1. Create Schedule
     let mut schedule = Schedule::default();
-    // 1a. Register stages.
-    schedule.add_stage(MAIN);
-    // 1b. Add systems to registered stages. Failure to register stages used in calls
-    //  to `add_system_to_stage` will result in a panic.
-    //  Here, `.system` is provided by `IntoQuerySystem`
+    // 2. Register stages.
+    schedule.add_stage(MAIN, SystemStage::parallel());
+    // 2b. Add systems to registered stages. Failure to register stages used in calls
+    // to `add_system_to_stage` will result in a panic.
+    // Here, `.system` is provided by `IntoQuerySystem`
     schedule.add_system_to_stage(MAIN, hello_world.system());
 
-    // 2. Create ParallelExecutor. The `without_tracker_clears` constructor means
-    // World and Resources will not clear resource/entity tracker state on each
-    // executor pass. This appears to refer to 'mutated' and 'added' flags?
-    let mut executor = ParallelExecutor::without_tracker_clears();
-    // 2a. Create World.
+    // 3. Create World.
     let mut world = World::default();
-    // 2b. Create Resources.
-    let mut resources = Resources::default();
-
-    // 3. Initialize the executor.
-    executor.initialize(&mut resources);
-    // 4. Initialize the scheduler.
-    schedule.initialize(&mut world, &mut resources);
-    // 5. Run the executor.
-    executor.run(&mut schedule, &mut world, &mut resources);
+    // 4. Run the executor.
+    schedule.run(&mut world);
 }
--- src/bin/01_entities.rs
+++ src/bin/01_entities.rs
@@ -1,28 +1,49 @@
-use bevy_ecs::{IntoForEachSystem, ParallelExecutor, Resources, Schedule, World};
+use bevy_ecs::prelude::*;
 
-const MAIN: &'static str = "MAIN";
+#[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
+enum Stages {
+    Startup,
+    Main,
+}
 
-fn hello_blank(name: &String) {
-    println!("hello {}", name);
+// Greet entities with a Name component.
+fn hello_blank(names: Query<&Name>) {
+    for name in names.iter() {
+        println!("Hello, {}", name.0);
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Name(pub String);
+
+impl Name {
+    pub fn new(s: &str) -> Self {
+        Name(s.to_owned())
+    }
+}
+
+impl Default for Name {
+    fn default() -> Self {
+        Name("NONAME".to_owned())
+    }
+}
+
+// Add some entities, populated with Name components, to the World.
+fn add_people(mut cmds: Commands) {
+    for name in ["Olive", "Lydia", "Bob", "Magnus"].iter() {
+        cmds.spawn().insert(Name::new(name));
+    }
 }
 
 fn main() {
     let mut schedule = Schedule::default();
-    schedule.add_stage(MAIN);
-    //  Here, `.system` is provided by `IntoForEachSystem`
-    //  The function signature of `hello_blank` determines the 'for each' in question
-    schedule.add_system_to_stage(MAIN, hello_blank.system());
 
-    let mut executor = ParallelExecutor::without_tracker_clears();
+    schedule.add_stage(Stages::Startup, SystemStage::parallel());
+    schedule.add_stage_after(Stages::Startup, Stages::Main, SystemStage::parallel());
+
+    schedule.add_system_to_stage(Stages::Startup, add_people.system());
+    schedule.add_system_to_stage(Stages::Main, hello_blank.system());
+
     let mut world = World::default();
-    let mut resources = Resources::default();
-
-    // Add some entities, populated with String components, to the World.
-    for name in ["Olive", "Lydia", "Bob", "Magnus"].iter() {
-        world.spawn((name.to_string(),));
-    }
-
-    executor.initialize(&mut resources);
-    schedule.initialize(&mut world, &mut resources);
-    executor.run(&mut schedule, &mut world, &mut resources);
+    schedule.run(&mut world);
 }
